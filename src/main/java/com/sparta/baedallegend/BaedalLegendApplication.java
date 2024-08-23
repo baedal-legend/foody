87922d49ef4163dc7d6cf6afe156631d0ca4d3cf
uzjaee <kyj960802@gmail.com>
2024-08-23 14:19:54 +0900

    init commit

---
+++ src/main/java/com/sparta/baedallegend/BaedalLegendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.baedallegend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalLegendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaedalLegendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/baedallegend/BaedalLegendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.baedallegend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalLegendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
