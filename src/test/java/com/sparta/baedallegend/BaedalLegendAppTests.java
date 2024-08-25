d44690a58bacce4d122acbad8f44c6755d388385
choi-ys <project.log.062@gmail.com>
2024-08-25 23:40:07 +0900

    style(#24): 가독성을 위한 EntryPoint 파일명 수정
    - AS-IS : *Application
    - TO-BE : *App

--- src/test/java/com/sparta/baedallegend/BaedalLegendAppTests.java
+++ src/test/java/com/sparta/baedallegend/BaedalLegendAppTests.java
@@ -2,9 +2,11 @@ package com.sparta.baedallegend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-class BaedalLegendApplicationTests {
+class BaedalLegendAppTests {
 
     @Test
     void contextLoads() {
