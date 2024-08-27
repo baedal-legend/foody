04e4e01ef9669e397cc79fc167509a4a0bccb1b2
choi-ys <project.log.062@gmail.com>
2024-08-27 16:42:51 +0900

    config(#12): Spring Security 기본 보안 정책 설정

---
+++ src/main/java/com/sparta/baedallegend/global/config/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.sparta.baedallegend.global.config.security;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+			.sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().authenticated()
+			)
+			.build();
+	}
+
+}
