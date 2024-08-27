679b10672b433b5abd0ed453feaa5f3431c46cf9
choi-ys <project.log.062@gmail.com>
2024-08-27 22:36:51 +0900

    feat:(#33): 로그인 API 구현
    - 로그인 요청 허용 인가 정책 추가
    - intellij http-client를 이용한 로그인 API 호출 TC 작성
    - @WebMvcTest를 이용한 로그인 API TC 작성

--- src/main/java/com/sparta/baedallegend/auth/controller/AuthController.java
+++ src/main/java/com/sparta/baedallegend/auth/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.sparta.baedallegend.auth.controller;
 
 import static com.sparta.baedallegend.global.config.utils.ResponseEntityUtils.created;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
+import com.sparta.baedallegend.auth.controller.model.SignInRequest;
 import com.sparta.baedallegend.auth.controller.model.SignUpRequest;
+import com.sparta.baedallegend.auth.service.AuthenticationService;
 import com.sparta.baedallegend.auth.service.SignUpFacade;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +27,7 @@ public class AuthController {
 
 	private static final String GET_AN_USER_URI = "/user/{id}";
 	private final SignUpFacade signUpFacade;
+	private final AuthenticationService authenticationService;
 
 	@PostMapping("/sign-up")
 	ResponseEntity<Void> signUp(@RequestBody SignUpRequest signUpRequest) {
@@ -30,6 +35,15 @@ public class AuthController {
 			GET_AN_USER_URI,
 			signUpFacade.signUp(signUpRequest)
 		);
+	}
+
+	@PostMapping("/sign-in")
+	ResponseEntity<Void> signIn(
+		@RequestBody SignInRequest signInRequest,
+		HttpServletResponse response
+	) {
+		response.setHeader(AUTHORIZATION, authenticationService.authenticate(signInRequest));
+		return ResponseEntity.ok().build();
 	}
 
 }
