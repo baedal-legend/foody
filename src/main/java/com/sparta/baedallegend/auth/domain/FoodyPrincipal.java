aa10a5e600e9cd379c1cd6fb173dd56ccbbff91a
choi-ys <project.log.062@gmail.com>
2024-08-27 22:35:36 +0900

    feat:(#33): access token 토큰 발급 부 구현
    - jwt 발급을 위한 jjwt 의존성 추가
    - jwt 발급에 필요한 환경 설정 구성
    - jwt 환경 설정 주입을 위한 @EnableConfigurationProperties 설정 추가
    - jwt 환경 설정을 주입 받는 객체 정의
    - jwt 발급 처리 부 구현

--- src/main/java/com/sparta/baedallegend/auth/domain/FoodyPrincipal.java
+++ src/main/java/com/sparta/baedallegend/auth/domain/FoodyPrincipal.java
@@ -2,15 +2,10 @@ package com.sparta.baedallegend.auth.domain;
 
 import com.sparta.baedallegend.user.domain.Role;
 
-public class FoodyPrincipal {
-
-	private Long id;
-	private Role role;
-
-	private FoodyPrincipal(Long id, Role role) {
-		this.id = id;
-		this.role = role;
-	}
+public record FoodyPrincipal(
+	Long id,
+	Role role
+) {
 
 	public static FoodyPrincipal of(Long id, Role role) {
 		return new FoodyPrincipal(id, role);
