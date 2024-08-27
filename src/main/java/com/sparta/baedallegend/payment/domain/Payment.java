530a04c54760fc5df7b0dd09b18631ae80b969e6
linavell <linavell@naver.com>
2024-08-27 21:07:43 +0900

    feat(#25): payAmount 타입 변경
    - 연산을 용이하게 하기 위하여 Long -> BigDecimal 타입 변경

--- src/main/java/com/sparta/baedallegend/payment/domain/Payment.java
+++ src/main/java/com/sparta/baedallegend/payment/domain/Payment.java
@@ -1,15 +1,21 @@
 package com.sparta.baedallegend.payment.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Entity
 @Getter
 @Table(name = "p_payment")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
 
     @Id
@@ -17,7 +23,7 @@ public class Payment {
     private UUID id;
 
     @Column(nullable = false)
-    private Long payAmount;
+    private BigDecimal payAmount;
 
     // TODO 연관관계 필요
 }
