0d07d995f5a582ed129e2bd130ae2b35764aa0f1
uzjaee <kyj960802@gmail.com>
2024-08-28 21:19:18 +0900

    chore(#39) 메뉴 등록 api controller url 수정 : /menu/ > /menu http client : 빈 주석 삭제

--- src/main/java/com/sparta/baedallegend/menu/controller/MenuController.java
+++ src/main/java/com/sparta/baedallegend/menu/controller/MenuController.java
@@ -4,6 +4,7 @@ import com.sparta.baedallegend.menu.controller.dto.CreateMenuRequest;
 import com.sparta.baedallegend.menu.service.MenuService;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +13,16 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MenuController {
 
 	private final MenuService menuService;
 
-	@PostMapping("/menu/")
+	@PostMapping("/menu")
 	public ResponseEntity<Void> createMenu(
 		@RequestBody CreateMenuRequest createMenuRequest
 	) {
+		log.info(createMenuRequest.toString());
 		final String menuId = menuService.create(createMenuRequest);
 
 		URI uri = UriComponentsBuilder
