304e12020e79498e471899ccf4c558826cf8398b
choi-ys <project.log.062@gmail.com>
2024-08-28 15:42:16 +0900

    config(#57): 데이터 감사 및 추적을 위한 Auditor 값 자동 설정 Configuration 구성
    JwtFilter를 거쳐 SecurityContextHolder에 저장된 인증된 사용자 정보를 Jpa Auditing에 사용할 수 있도록 JpaAuditingAware 환경 설정 구성

---
+++ src/main/java/com/sparta/baedallegend/global/config/jpa/AuditorAwareConfig.java
@@ -0,0 +1,33 @@
+package com.sparta.baedallegend.global.config.jpa;
+
+import static org.springframework.security.config.Elements.ANONYMOUS;
+
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+public class AuditorAwareConfig {
+
+	@Bean
+	public AuditorAware<Long> auditorAware() {
+		return () -> Optional.of(SecurityContextHolder.getContext())
+			.map(SecurityContext::getAuthentication)
+			.filter(this::isAuthenticated)
+			.map(Authentication::getPrincipal)
+			.map(Long.class::cast);
+	}
+
+	private boolean isAuthenticated(Authentication authentication) {
+		return authentication.isAuthenticated() && isNotAnonymousUser(authentication);
+	}
+
+	private boolean isNotAnonymousUser(Authentication authentication) {
+		return !authentication.getName().contains(ANONYMOUS);
+	}
+
+}
