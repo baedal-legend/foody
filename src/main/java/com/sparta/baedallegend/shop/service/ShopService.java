29f2c54601de3fcd4c1989f2a258b16a439626e6
linavell <linavell@naver.com>
2024-08-29 14:51:06 +0900

    feat(#47): Shop 단일 조회 API 구현
    - 메뉴의 상태값 설명을 반환하기 위해 getter 메서드 추가
    - 단일 조회에 필요한 Repo, Service, Controller 구현
    - isPublic이 true인 메뉴들만 조회

--- src/main/java/com/sparta/baedallegend/shop/service/ShopService.java
+++ src/main/java/com/sparta/baedallegend/shop/service/ShopService.java
@@ -2,10 +2,15 @@ package com.sparta.baedallegend.shop.service;
 
 import com.sparta.baedallegend.category.domain.Category;
 import com.sparta.baedallegend.category.repo.CategoryRepo;
+import com.sparta.baedallegend.menu.domain.Menu;
+import com.sparta.baedallegend.menu.repo.MenuRepo;
 import com.sparta.baedallegend.region.domain.Region;
 import com.sparta.baedallegend.region.repo.RegionRepo;
 import com.sparta.baedallegend.shop.controller.dto.CreateShopRequest;
 import com.sparta.baedallegend.shop.controller.dto.FindAllShopResponse;
+import com.sparta.baedallegend.shop.controller.dto.ReadOneShopMenuResponse;
+import com.sparta.baedallegend.shop.controller.dto.ReadOneShopRequest;
+import com.sparta.baedallegend.shop.controller.dto.ReadOneShopResponse;
 import com.sparta.baedallegend.shop.domain.Shop;
 import com.sparta.baedallegend.shop.domain.ShopCategory;
 import com.sparta.baedallegend.shop.repo.ShopCategoryRepo;
@@ -32,6 +37,7 @@ public class ShopService {
 	private final CategoryRepo categoryRepo;
 	private final ShopCategoryRepo shopCategoryRepo;
 	private final RegionRepo regionRepo;
+	private final MenuRepo menuRepo;
 
 	@Transactional
 	public String create(Long userId, CreateShopRequest createShopRequest) {
@@ -67,5 +73,19 @@ public class ShopService {
 
 		return new PageImpl<>(findAllShopResponses, pageRequest, shops.getSize());
 	} // TODO 페이징 처리 필요
+
+	public ReadOneShopResponse readOne(ReadOneShopRequest request) {
+		Shop shop = shopRepo.findById(UUID.fromString(request.getShopId())).orElseThrow(() ->
+			new IllegalArgumentException("존재하지 않는 상점입니다."));
+
+		List<Menu> menus = menuRepo.findByShopAndIsPublicTrue(shop);
+
+		List<ReadOneShopMenuResponse> menuResponses = menus.stream()
+			.map(ReadOneShopMenuResponse::from).collect(
+				Collectors.toList());
+
+		ReadOneShopResponse response = ReadOneShopResponse.of(shop, menuResponses);
+		return response;
+	}
 
 }
