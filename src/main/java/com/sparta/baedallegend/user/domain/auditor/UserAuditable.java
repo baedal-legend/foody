f640fcb5662418857a421559fb476a7afa371f2f
choi-ys <project.log.062@gmail.com>
2024-08-30 01:13:41 +0900

    fix(#70): Audit 정책 변경에 따른 UserAuditable 변경
    - 기존 @Embedded 구조에서 상속 구조로 변경
    - 중복 적용된 AuditingEntityListener 설정 제거

--- src/main/java/com/sparta/baedallegend/user/domain/auditor/UserAuditable.java
+++ src/main/java/com/sparta/baedallegend/user/domain/auditor/UserAuditable.java
@@ -1,20 +1,15 @@
 package com.sparta.baedallegend.user.domain.auditor;
 
 import com.sparta.baedallegend.global.config.jpa.CommonAuditFields;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class UserAuditable {
+public abstract class UserAuditable extends CommonAuditFields {
 
 	protected Long createdBy;
 	protected LocalDateTime createdAt;
-
-	protected CommonAuditFields auditableFields;
 
 }
